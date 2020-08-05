55bb85ce8f028e74f97b2712cd377315b0fcb3ee
andre silva <andre.silvajb@gmailcom>
2020-08-04 23:22:56 -0300

    subindo

---
+++ Agenda/Alunos/Controller/AlunoViewController.swift
@@ -0,0 +1,61 @@
+//
+//  AlunoViewController.swift
+//  Agenda
+//
+//  Created by Ândriu Coelho on 24/11/17.
+//  Copyright © 2017 Alura. All rights reserved.
+//
+
+import UIKit
+
+class AlunoViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var viewImagemAluno: UIView!
+    @IBOutlet weak var imageAluno: UIImageView!
+    @IBOutlet weak var buttonFoto: UIButton!
+    @IBOutlet weak var scrollViewPrincipal: UIScrollView!
+    
+    @IBOutlet weak var textFieldNome: UITextField!
+    @IBOutlet weak var textFieldEndereco: UITextField!
+    @IBOutlet weak var textFieldTelefone: UITextField!
+    @IBOutlet weak var textFieldSite: UITextField!
+    @IBOutlet weak var textFieldNota: UITextField!
+    
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.arredondaView()
+        NotificationCenter.default.addObserver(self, selector: #selector(aumentarScrollView(_:)), name: .UIKeyboardWillShow, object: nil)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // MARK: - Métodos
+    
+    func arredondaView() {
+        self.viewImagemAluno.layer.cornerRadius = self.viewImagemAluno.frame.width / 2
+        self.viewImagemAluno.layer.borderWidth = 1
+        self.viewImagemAluno.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    @objc func aumentarScrollView(_ notification:Notification) {
+        self.scrollViewPrincipal.contentSize = CGSize(width: self.scrollViewPrincipal.frame.width, height: self.scrollViewPrincipal.frame.height + self.scrollViewPrincipal.frame.height/2)
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func buttonFoto(_ sender: UIButton) {
+        // TO DO
+    }
+    
+    @IBAction func stepperNota(_ sender: UIStepper) {
+        self.textFieldNota.text = "\(sender.value)"
+    }
+    
+    
+}
